7b709cf568eea0cbc493d58718f125bd933f73df
bo cheng <cgs05521@gmail.com>
2019-12-21 11:19:07 +0800

    add America

--- driving.py
+++ driving.py
@@ -3,6 +3,11 @@ age = input('請輸入年齡： ')
 age = int(age)
 if country == '台灣' :
 	if age >=18:
-		print('你可以考駕照')
+		print('你可以考台灣駕照')
 	else :
-		print('你不能考駕照')
+		print('你不能考台灣駕照')
+elif country == '美國':
+	if age >=16:
+		print('你可以考美國駕照')
+	else:
+		print('你不能考美國駕照')
